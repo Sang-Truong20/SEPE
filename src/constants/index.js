748de7b109d9dfb832be03661e8d6725e2f7dfe6
Sang Truong <sangtpse171049@fpt.edu.vn>
2025-09-24 19:47:39 +0700

    add auth (authorization + authentication)

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,30 +1,9 @@
 const PATH_NAME = {
   HOME: '/',
   AUTH: '/auth',
-  POST: '/post',
-  POST_DETAIL: '/post/:id',
   NOT_FOUND: '*',
   MEMBER: '/member',
   ADMIN: '/admin',
 };
 
-const NAV_ELEMENTS = [
-  {
-    name: 'TRANG CHỦ',
-    path: '/',
-  },
-  {
-    name: 'BÀI VIẾT',
-    path: '#',
-  },
-  {
-    name: 'TRỢ LÝ ẢO',
-    path: '#',
-  },
-  {
-    name: 'VỀ CHÚNG TÔI',
-    path: '/contact',
-  },
-];
-
-export { PATH_NAME, NAV_ELEMENTS };
+export { PATH_NAME };
