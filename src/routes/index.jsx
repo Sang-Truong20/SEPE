b2aec7c34307b6f60e48e8216ca05871e810b605
soilangtu <dung3047@gmail.com>
2025-09-24 10:20:01 +0700

    refactor(routes): remove GuestRoute and AuthPage from index.jsx
    - Eliminated the GuestRoute and its associated AuthPage from the routing configuration.
    - Streamlined the router setup for improved clarity and maintainability.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,17 +3,14 @@ import { lazy, Suspense } from 'react';
 import { createBrowserRouter, Outlet } from 'react-router-dom';
 import LandingLayout from '../components/layouts/LandingLayout';
 import { PATH_NAME } from '../constants';
-import AuthPage from '../pages/auth';
 import NotFound from '../pages/notfound';
 import AdminRoutes from './AdminRoutes';
-import GuestRoute from './GuestRoute';
 import MemberRoutes from './MemberRoutes';
 
 const LandingPage = lazy(() => import('../pages/landing'));
 const MemberPage = lazy(() => import('../pages/member'));
 const AdminPage = lazy(() => import('../pages/admin'));
 const AdminChallengeCreatePage = lazy(() => import('../pages/admin/challenge-create'));
-
 const withSuspense = (Component) => (
   <Suspense
     fallback={
@@ -50,16 +47,6 @@ const router = createBrowserRouter([
       },
     ],
   },
-  {
-    element: <GuestRoute />,
-    children: [
-      {
-        path: PATH_NAME.AUTH,
-        element: <AuthPage />,
-      },
-    ],
-  },
-
   {
     element: <AdminRoutes />,
     children: [
