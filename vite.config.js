e236766e9a75c2405d98d91d6a5d76003dec08da
soilangtu <134293705+soilangtu@users.noreply.github.com>
2025-12-11 11:26:01 +0700

    Optimize build: add code-splitting and Vercel config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,30 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'antd',
+            '@ant-design/icons',
+          ],
+          'firebase': [
+            'firebase/app',
+            'firebase/auth',
+            'firebase/firestore',
+            'firebase/messaging',
+          ],
+          'utils': [
+            'axios',
+            'js-cookie',
+          ],
+        },
+      },
+    },
+  },
 });
